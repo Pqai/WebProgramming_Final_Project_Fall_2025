ecf4f426dbc55b51d18cc6b0b601b96e7515397d
Jiego Pasamanero <83421019+Pqai@users.noreply.github.com>
2025-10-31 20:31:41 -0600

    viewcoursesPage added

---
+++ sodv2201-finalproject/src/views/student/ViewCourses.jsx
@@ -0,0 +1,11 @@
+import CourseCardList from "../../components/ui/CourseCardList/coursecardlist.component"
+
+const ViewCoursesPage = () =>{
+    return(
+        <div>
+            <CourseCardList />
+        </div>
+    );
+};
+
+export default ViewCoursesPage;
